cbad9d68db6ed89bea09c12f10561026376e2d25
rcemper <31236645+rcemper@users.noreply.github.com>
2022-12-02 18:10:33 +0100

    Update irissession.sh

--- irissession.sh
+++ irissession.sh
@@ -9,8 +9,6 @@ if '\$Get(sc) do ##class(%SYSTEM.Process).Terminate(, 1)
 zn "%SYS"
 do ##class(SYS.Container).QuiesceForBundling()
 Do ##class(Security.Users).UnExpireUserPasswords("*")
-ZN "IRISAPP"
-zpm "install webterminal"
 halt
 EOF
 
